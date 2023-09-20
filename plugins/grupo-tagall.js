ec5eb5946f6fe92850067813a30a323e39015ca0
4nni33 <108834497+4nni33@users.noreply.github.com>
2023-09-20 09:00:16 -0300

    Update grupo-tagall.js

--- plugins/grupo-tagall.js
+++ plugins/grupo-tagall.js
@@ -6,9 +6,11 @@ throw false
 let vn = './media/Invocar.mp3'
 let pesan = args.join` `
 let oi = `*@lunita_2art* ${pesan}`
-let teks = `*¡ATENCIÓN!*\n\n ${oi}\n\n ➢ 𝙴𝚃𝙸𝚀𝚄𝙴𝚃𝙰𝚂\n`
+let teks = `*¡ATENCIÓN!*\n\n ${oi}\n ➢ 𝙴𝚃𝙸𝚀𝚄𝙴𝚃𝙰𝚂\n`
+teks += `─ ⋯ ─ ⋯ ─ ⋯ ─ ⋯ ─ `
 for (let mem of participants) {
 teks += `🫧໋᳝݊▹֛◌  @${mem.id.split('@')[0]}\n`}
+  teks += `─ ⋯ ─ ⋯ ─ ⋯ ─ ⋯ ─ `
 teks += `🌸ɞ  𝗦𝗼𝗳𝘅𝗕𝗼𝘁 ✨ ⤾·˚ ༘*`
 conn.sendMessage(m.chat, { text: teks, mentions: participants.map(a => a.id) }, )
 conn.sendFile(m.chat, vn, 'Invocar.mp3', null, m, true, { type: 'audioMessage', ptt: true, sendEphemeral: true })
