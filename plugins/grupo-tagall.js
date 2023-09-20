29f8f540c85e2d09a2381be68cd17e5c7e451cb3
4nni33 <108834497+4nni33@users.noreply.github.com>
2023-09-20 09:08:59 -0300

    Update grupo-tagall.js

--- plugins/grupo-tagall.js
+++ plugins/grupo-tagall.js
@@ -6,7 +6,7 @@ throw false
 let vn = './media/Invocar.mp3'
 let pesan = args.join` `
 let oi = `*@lunita_2art* ${pesan}`
-let teks = `*¡ATENCIÓN!*\n\n ${oi}\n ➢ 𝙴𝚃𝙸𝚀𝚄𝙴𝚃𝙰𝚂\n`
+let teks = `*¡ATENCIÓN!*\n\n ${oi}\n ㅤﻬ 𝙴𝚃𝙸𝚀𝚄𝙴𝚃𝙰𝚂\n`
 teks += `─ ⋯ ─ ⋯ ─ ⋯ ─ ⋯ ─\n `
 for (let mem of participants) {
 teks += `🫧໋᳝݊▹֛◌  @${mem.id.split('@')[0]}\n`}
